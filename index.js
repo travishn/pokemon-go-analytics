baf2e34a64c22f60453106a84c881f15a94f3309
Travis <travishn@uci.edu>
2018-08-06 00:36:46 -0700

    Add env package

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const pg = require('pg');
@@ -10,19 +11,47 @@ const PORT = process.env.PORT || 5000;
 // const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/pogo_analytics';
 // const client = new pg.Client(connectionString);
 const config = {
-  host: 'localhost',
-  port: 5432,
-  database: 'pogo_analytics',
+  host: process.env.HOST,
+  port: process.env.DBPORT,
+  database: process.env.DB,
 };
 
 const pool = new pg.Pool(config);
 
 app.get('/', (req, res) => {
-  res.send({ express: 'Hello from Express!' });
+  res.send({
+    express: 'Hello from Express!',
+    allPokemon: '/pokemon',
+    pokemonById: '/pokemon/:pokemonId',
+    pokemonByGen: './pokemon/generation/:generationId',
+  });
 });
 
 app.get('/pokemon', async (req, res) => {
   const response = await pool.query('SELECT * FROM pokemon');
+  res.send(response.rows);
+});
+
+app.get('/pokemon/:pokemonId', async (req, res) => {
+  const pokeId = req.params.pokemonId;
+  const response = await pool.query(`SELECT * FROM pokemon WHERE pokemon.id = ${pokeId}`);
+  res.send(response.rows);
+});
+
+app.get('/pokemon/generation/:generationId', async (req, res) => {
+  const gen = req.params.generationId;
+  const response = await pool.query(`SELECT * FROM pokemon WHERE pokemon.generation = ${gen}`);
+  res.send(response.rows);
+});
+
+app.get('/pokemon/type/:typeElement', async (req, res) => {
+  const type = req.params.typeElement;
+  const response = await pool.query(
+    `SELECT * 
+    FROM pokemon 
+    WHERE pokemon.type1 = ${type}
+    OR pokemon.type2 = ${type}`
+  );
   res.send(response.rows);
 });
 
