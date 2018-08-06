6d6837868bc97b1531daf9dd33c0f7ca930868e0
Travis <travishn@uci.edu>
2018-08-06 00:13:04 -0700

    Add all pokemon express route

--- index.js
+++ index.js
@@ -7,11 +7,23 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 5000;
-const connectionStr = process.env.DATABASE_URL || 'postgres://localhost:5432/pogo_analytics';
-const client = new pg.Client(connectionStr);
+// const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/pogo_analytics';
+// const client = new pg.Client(connectionString);
+const config = {
+  host: 'localhost',
+  port: 5432,
+  database: 'pogo_analytics',
+};
+
+const pool = new pg.Pool(config);
 
 app.get('/', (req, res) => {
   res.send({ express: 'Hello from Express!' });
+});
+
+app.get('/pokemon', async (req, res) => {
+  const response = await pool.query('SELECT * FROM pokemon');
+  res.send(response.rows);
 });
 
 app.listen(PORT, () => {
