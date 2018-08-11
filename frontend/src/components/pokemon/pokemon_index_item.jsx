f682464e93bb43cb3900e0ca090fa25a9c7a3943
Travis <travishn@uci.edu>
2018-08-10 20:37:32 -0700

    Add types and image

--- frontend/src/components/pokemon/pokemon_index_item.jsx
+++ frontend/src/components/pokemon/pokemon_index_item.jsx
@@ -5,7 +5,16 @@ const PokemonIndexItem = ({ pokemon }) => {
   return (
     <li className='poke-stats'>
       <div className='tb-entry'>{pokemon.id}</div>
-      <div className='tb-entry'>{pokemon.name}</div>
+      <div className='tb-entry'>
+        <img src={pokemon.image} />
+        <div className='name-type-wrapper'>
+          <h3>{pokemon.name}</h3>
+          <div className='type-container'>
+            <h3>{pokemon.type1}</h3>
+            <h3>{pokemon.type2}</h3>
+          </div>
+        </div>
+      </div>
       <div className='tb-entry'>{pokemon.cp}</div>
       <div className='tb-entry'>{pokemon.attack}</div>
       <div className='tb-entry'>{pokemon.defense}</div>
